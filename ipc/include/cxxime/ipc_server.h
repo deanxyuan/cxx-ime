f71e6e420b63ea6e42cd838c4c36c2bbb9272de1
shadow-yuan <shadow_yuan@qq.com>
2026-05-24 19:54:17 +0800

    refactor: redesign IPC with IOCP and overlapped I/O

--- ipc/include/cxxime/ipc_server.h
+++ ipc/include/cxxime/ipc_server.h
@@ -9,6 +9,7 @@
 #include <vector>
 #include <mutex>
 #include <atomic>
+#include <windows.h>
 #include <cxxime/ipc_protocol.h>
 
 namespace cxxime {
@@ -20,23 +21,38 @@ public:
     IpcServer() = default;
     ~IpcServer();
 
+    IpcServer(const IpcServer&) = delete;
+    IpcServer& operator=(const IpcServer&) = delete;
+
     bool start(const std::wstring& pipe_name = IPC_PIPE_BASE_NAME);
     void stop();
     void set_handler(RequestHandler handler);
 
 private:
-    void listen_loop();
-    void handle_client(void* pipe);
+    struct ClientContext {
+        HANDLE pipe = nullptr;
+        OVERLAPPED ol = {};
+        IPCRequest request = {};
+        IPCResponse response = {};
+        bool read_pending = true;
+    };
+
+    void accept_loop();
+    void worker_loop();
+    void add_context(ClientContext* ctx);
+    void remove_context(ClientContext* ctx);
+    void cleanup_client(ClientContext* ctx);
 
     std::wstring pipe_name_;
     RequestHandler handler_;
-    std::mutex handler_mutex_;
 
-    std::thread listen_thread_;
-    std::vector<std::thread> client_threads_;
-    std::mutex threads_mutex_;
-
+    HANDLE iocp_ = nullptr;
+    std::thread accept_thread_;
+    std::vector<std::thread> workers_;
     std::atomic<bool> running_{false};
+
+    std::mutex contexts_mutex_;
+    std::vector<ClientContext*> contexts_;
 };
 
 } // namespace cxxime
