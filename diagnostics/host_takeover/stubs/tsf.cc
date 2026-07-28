d2ee0fc0194c6bc0c5336e59e46cef40cc75801f
DeanYuan <xdingyuan@outlook.com>
2026-07-28 17:12:52 +0800

    fix: 收敛 TSF 分类并隔离普通宿主诊断观察器

--- diagnostics/host_takeover/stubs/tsf.cc
+++ diagnostics/host_takeover/stubs/tsf.cc
@@ -36,7 +36,7 @@ void stop_stage_runtime(const HostClassificationCompatibilitySnapshot&) {}
 
 void trace_stage_conversion_compartment(bool, HRESULT, DWORD, DWORD, bool, HRESULT) {}
 
-void start_stage_ui_element_observer(ITfThreadMgr*) {}
+void start_stage_ui_element_observer(ITfThreadMgr*, DWORD) {}
 
 void trace_stage_runtime_activate(DWORD, TfClientId) {}
 
